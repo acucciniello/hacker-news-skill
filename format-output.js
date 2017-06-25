f8d26e59001bbc0f6ca66efd9e823fca49541754
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-06-25 19:57:01 -0400

    Update to give 5 articles

--- format-output.js
+++ format-output.js
@@ -2,7 +2,7 @@ module.exports = formatOutput
 
 function formatOutput (response) {
   var output = 'Here are your Articles: '
-  for (var i = 0; i < (3 || response.articles.length); i++) {
+  for (var i = 0; i < (5 || response.articles.length); i++) {
     var articleCount = i + 1
     if (response.articles[i].title !== null && response.articles[i].description !== null) {
       output = output + articleCount + '. ' + response.articles[i].title + ' <break time="500ms" /> ' + response.articles[i].description + ' <break time="500ms" />'
